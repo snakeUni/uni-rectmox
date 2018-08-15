2458799f7e5208b4ef979186f10890f4b2afeddd
Uni <1025687605@qq.com>
2018-08-15 11:37:36 +0800

    add combine

--- src/store.js
+++ src/store.js
@@ -4,12 +4,26 @@ class Store {
     this.state = {}
     this.reducers = {}
     this.effects = {}
-    this.dispatch  = this.dispatch.bind(this)
+    // 通过dispatch来改变值,用法dispatch.count.increase
+    this.dispatch  = {}
   }
   init(models) {
     this.state = models.state || {}
     this.reducers = models.reducers || {}
     this.effects = models.effects || {}
+    this.dispatch = new Store() || {}
   }
 
+  subscribe(listener) {
+    if (typeof listener !== 'function') {
+      throw new Error('Expected listener to be a function')
+    }
+    const that = this
+    this.subscribers.push(callback)
+    return function() {
+      that.subscribes.filter(sub => sub !== callback)
+    }
+  }
+
+  
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,17 @@
 function combineModels(models = {}) {
-  
+  const state = {}
+  const reducers = {}
+  const effects = {}
+  Object.keys(models).forEach(key => {
+    if (models[key]) {
+      state[key] = models[key].state
+      reducers[key] = models[key].reducers
+      effects[key] = models[key].effects
+    }
+  })
+  return {
+    state,
+    reducers,
+    effects
+  }
 }
