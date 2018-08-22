0a85feb96d38c0fab84069d3fd9ed91dacca00e1
Uni <1025687605@qq.com>
2018-08-22 23:52:41 +0800

    change

--- src/index.js
+++ src/index.js
@@ -16,13 +16,39 @@ export class Provider extends Component {
   }
 }
 
-export const map = (state = {}, reducers = {}, effects = {}) => Component => {
+export const map = (namespace, state = [], reducers = [], effects = []) => Component => {
   return class wrapComponent extends Component {
     constructor(props) {
       super(props)
       this.state = {
         mapProps: {}
       }
+      this.namespace = namespace
+    }
+    mapStateToProps = () => {
+      const stateToProps = mapProps(this.store.state, state, this.namespace)
+      return stateToProps
+    }
+
+    mapReducersToProps = () => {
+      const reducersToProps = mapDispatch(this.store.reducers, reducers, this.namespace)
+      return reducersToProps
+    }
+
+    mapEffectsToProps = () => {
+      const effectsToProps = mapDispatch(this.store.effects, effects, this.namespace)
+      return effectsToProps
+    }
+    
+    render() {
+      return (
+        <Consumer>
+          {store => {
+            this.store = store
+            return <Component {...this.state.mapProps} />
+          }}
+        </Consumer>
+      )
     }
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -9,4 +9,23 @@ function resolveAction(action = {}) {
   }
 }
 
+function mapProps(storeText, currentState, namespace) {
+  if (typeof namespace !== 'string') {
+    throw new Error('namespace must be string')
+  }
+  if (Object.toString.call(currentState) !== '[object Array]') {
+    throw new Error('state must be a array')
+  }
+  const currentStoreState = storeText[namespace]
+  const result = {}
+  currentState.forEach(state => {
+    result[state] = currentState[state]
+  })
+  return result
+}
+
+function mapDispatch(storeDispatches, currentDispatch, namespace) {
+  
+}
+
 export { resolveAction }
