2beb123c66b95467d1b0a6307c2d14562c479c91
Uni <1025687605@qq.com>
2018-09-02 22:24:55 +0800

    增加相关测试

--- src/init.js
+++ src/init.js
@@ -3,6 +3,10 @@ import Store from './store'
 function init(models = {}) {
   const store = new Store();
   const keys = Object.keys(models);
+  const newKeys = [...new Set(keys)]
+  if (newKeys.length !== keys.length) {
+    throw new Error('repeated model name')
+  }
   keys.forEach(key => {
       store.state[key] = models[key].state || {};
       store.reducers[key] = models[key].reducers || {};
---
+++ tests/index-test.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import  { expect } from 'chai'
+import { init, Provider, observe } from '../src/index'
+
+describe('test for remonx', () => {
+  
+})
+
